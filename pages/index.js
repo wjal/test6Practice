c7d7a0d611fde2e1827107b547cd8280175b2d42
wjal <wjalittle@myseneca.ca>
2023-04-10 23:56:04 -0400

    Fix - restored vercel.com link

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
           </p>
           <div>
             <a
-              href="https://abc.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
               >
